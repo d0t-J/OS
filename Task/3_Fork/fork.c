c24fef4c1decaa50d8f3a87a283b9ecaefd788c9
Junaid <j.ahmed.ja7a7@gmail.com>
2024-12-24 09:49:07 +0500

    Fork.c: Added Comments + Removed Misc.

--- Task/3_Fork/fork.c
+++ Task/3_Fork/fork.c
@@ -4,10 +4,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define SIZE 5
-
-int nums[SIZE] = {0, 1, 2, 3, 4};
-
 int main()
 {
     int n;
@@ -18,10 +14,15 @@ int main()
     } while (n <= 0);
 
     printf("Existing Parent ID: %d\n", getpid());
+    // * This statement Prints out the parent ID of the current existing process alloted to the running program.
+    // * For the sake of understanding the pid that will be printed by this statement will be of the VS Code instance at the run time or the compiled file of this code during its runtime.
+    // * Next thing we want to do is to create n number of child processes from the same parent.
 
     for (int i = 0; i < n; i++)
     {
         pid_t childP = fork();
+        // * This fork will be called n times and will create n number of child processes.
+        // * The child process will be alloted a different process ID of its own but they all will have the same parent process ID.
 
         if (childP < 0)
         {
@@ -34,10 +35,12 @@ int main()
             printf("Child Process ID: %d\t of Parent Process ID: %d\n", getpid(), getppid());
             exit(0);
             continue;
+            // * Successfully terminates the child process that is created and returns to the original parent process.
+            // * For the next iteration, the same parent will fork another child process.
         }
+        // * This else block can be used to check if for the i-th iteration of the loop, the child processes has the same parent ID or not.
         // else
         // {
-        //     // printf(" ChID: %d   Parent Process ID: %d\n", getpid(), getppid());
         //     printf(" ChID: %d\n ", getpid());
         // }
     }
@@ -45,7 +48,7 @@ int main()
     {
         wait(NULL);
     }
-    //  printf("Parent Process ID: %d\n", getpid());
-
+    // * At the end of the program, the parent process should remain alive (ln: 16) and print its own process ID.
+    printf("Parent Process ID: %d\n", getpid());
     return 0;
 }
